0aeaeab1675e6d06b9d292ea5cc6599fbc0d577e
alex <alex96ford19@gmail.com>
2020-01-15 15:23:23 +0000

    removed unneeded code and added addsession.html with form

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-
+@app.route('/addsession')
+def addsession():
+    return render_template("addsession.html")
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
