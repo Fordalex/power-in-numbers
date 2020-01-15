e8acc9f3b999460af4b4b0fa8feee4b3c62c3ba0
alex <alex96ford19@gmail.com>
2020-01-15 00:56:41 +0000

    Added bootstrap, navigation bar, readme.md, and sass

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
-import flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-app.config["MONGO_######"] = ' #####'
-app.config["MONGO_URI"] = '#####'
 
-mongo = PyMongo(app) 
 
-@app.route('/home')
-def home():
-    return render_template("#####.html", #####=mongo.db.#####.find())
-if __name__ -- '__main__':
-    app.run(host-os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+
+@app.route('/')
+def index():
+    return render_template("base.html")
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv("IP", "0.0.0.0"),
+            port=int(os.getenv("PORT", "5000")), debug=True)
