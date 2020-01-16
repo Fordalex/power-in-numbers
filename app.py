ba13908b92ff75e0e1de0681cf932300a57fa6bf
alex <alex96ford19@gmail.com>
2020-01-16 13:12:28 +0000

    created the DB and linked mongoDB to this project

--- app.py
+++ app.py
@@ -2,17 +2,32 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+pinDB = os.environ.get('pinDB') 
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = pinDB
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", sessions=mongo.db.sessions.find())
 
 @app.route('/addsession')
 def addsession():
     return render_template("addsession.html")
+
+@app.route('/insert_session', methods=['POST'])
+def insert_session():
+    sessions = mongo.db.sessions
+    sessions.insert_one(request.form.to_dict())
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
