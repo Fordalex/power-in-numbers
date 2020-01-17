35e59d8b2d56654845f0e0b9b22b571a2551c963
alex <alex96ford19@gmail.com>
2020-01-17 17:50:22 +0000

    filter front-end, moved content in session cards, expand buttons toggle label

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,3 +1,4 @@
+// Adds a row for the tables with changing name attribute
 var rowIndex = 1;
 
 function createRow() {
@@ -33,3 +34,14 @@ function tableColour(num) {
 for (k = 0; k < newYearTable.length; k++) {
     tableColour(k);
 }
+
+// changing the expand text
+$('.expand-button').html('Expand')
+$('.expand-button').on('click', function() {
+    console.log(this.text)
+    if (this.text == 'Expand') {
+        $(this).html('Close');
+    } else {
+        $(this).html('Expand');
+    }
+})
