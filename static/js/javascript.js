44de880269295020bcef9f1d66bacb7ac930558b
alex <alex96ford19@gmail.com>
2020-01-17 14:41:41 +0000

    created js file, 'add row' button now works with name attribute. small style and margins fixed.

---
+++ static/js/javascript.js
@@ -0,0 +1,35 @@
+var rowIndex = 1;
+
+function createRow() {
+    rowIndex++;
+    $('#addSessionTable').append(` <tr>
+    <td><input class="container-fluid" type="text" name="session_exercise_${rowIndex}"></td>
+    <td><input class="container-fluid" type="type" name="session_sets_${rowIndex}"></td>
+    <td><input class="container-fluid" type="type" name="session_weight_${rowIndex}"></td>
+</tr>`)
+    for (k = 0; k < newYearTable.length; k++) {
+        tableColour(k);
+    }
+};
+
+// Styles the session table
+var newYearTable = document.getElementsByClassName('session-table');
+var mainColour = 'rgb(136, 227, 255)';
+var firstColour = 'rgb(220,220,220)';
+var secondaryColour = 'rgb(240,240,240)';
+
+function tableColour(num) {
+    var newYearTableRow = newYearTable[num].getElementsByTagName('tr');
+    for (i = 0; i < newYearTableRow.length; i++) {
+        if (i == 0) {
+            newYearTableRow[i].classList.add('session-header');
+        } else if (i % 2 == 0) {
+            newYearTableRow[i].style.backgroundColor = firstColour;
+        } else {
+            newYearTableRow[i].style.backgroundColor = secondaryColour;
+        }
+    };
+};
+for (k = 0; k < newYearTable.length; k++) {
+    tableColour(k);
+}
