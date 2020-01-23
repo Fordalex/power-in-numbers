fe0d0e87c4d1ac66e04f8fc7eb82f2ca2a765605
alex <alex96ford19@gmail.com>
2020-01-23 03:12:43 +0000

    more dynamic form experience for the user.

--- app.py
+++ app.py
@@ -93,45 +93,8 @@ def add_session():
 @app.route('/insert_session', methods=['POST'])
 def insert_session():
     sessions = mongo.db.sessions
-    age = request.form['age']
-    body_weight = request.form['body_weight']
-    bw_unit = request.form['bw_unit']
-    date = request.form['date']
-    difficulty = request.form['difficulty']
-    effort = request.form['effort']
-    first_name = request.form['first_name']
-    gender = request.form['gender']
-    last_name = request.form['last_name']
-    length_hour = request.form['length_hour']
-    length_min = request.form['length_min']
-    location = request.form['location']
-    motivated = request.form['motivated']
-    notes = request.form['notes']
-    session_type = request.form['session_type']
-    session_exercise_1 = request.form['session_exercise_1']
-    session_sets_1 = request.form['session_sets_1']
-    session_weight_1 = request.form['session_weight_1']
-    weight_unit = request.form['weight_unit']
-    sessionDict = {
-                    "age": age, 
-                    "body_weight": body_weight, 
-                    "bw_unit": bw_unit, 
-                    "date": date, 
-                    "difficulty": difficulty, 
-                    "effort": effort, 
-                    "first_name": first_name, 
-                    "gender": gender, 
-                    "last_name": last_name, 
-                    "length_hour": length_hour, 
-                    "length_min": length_min, 
-                    "location": location, 
-                    "motivated": motivated, 
-                    "notes": notes, 
-                    "session_exercise": [{'session_exercise_1' : session_exercise_1,'session_sets_1':session_sets_1,'session_weight_1': session_weight_1 },{'session_exercise_2' : session_exercise_1,'session_sets_1':session_sets_1,'session_weight_1': session_weight_1 }],
-                    "session_type": "Powerlifting", 
-                    "weight_unit": "lb"
-}
-    sessions.insert_one(sessionDict)
+    sessionForm = request.form.to_dict()
+    sessions.insert_one(sessionForm)
     return redirect(url_for('home'))
 
 ############################################
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
     fixFloat()
 })
 
- // Welcome message
- var today = new Date();
- var hourNow = today.getHours();
- var greeting;
+// Welcome message
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
- if (hourNow > 20) {
-     greeting = 'Good Evening'
- } else if (hourNow > 12) {
-     greeting = 'Good Afternoon'
- } else {
-     greeting = 'Good Morning'
- };
+if (hourNow > 20) {
+    greeting = 'Good Evening'
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon'
+} else {
+    greeting = 'Good Morning'
+};
 
- $('#greetingMessage').html(greeting);
+$('#greetingMessage').html(greeting);
 
 // Adds a row for the tables with changing name attribute
 var rowIndex = 1;
@@ -34,13 +34,13 @@ function createRow() {
 };
 
 // Styles the session table
-var newYearTable = document.getElementsByClassName('session-table');
+var sessionTable = document.getElementsByClassName('session-table');
 var mainColour = 'rgb(136, 227, 255)';
 var firstColour = 'rgb(220,220,220)';
 var secondaryColour = 'rgb(240,240,240)';
 
 function tableColour(num) {
-    var newYearTableRow = newYearTable[num].getElementsByTagName('tr');
+    var newYearTableRow = sessionTable[num].getElementsByTagName('tr');
     for (i = 0; i < newYearTableRow.length; i++) {
         if (i == 0) {
             newYearTableRow[i].classList.add('session-header');
@@ -51,13 +51,13 @@ function tableColour(num) {
         }
     };
 };
-for (k = 0; k < newYearTable.length; k++) {
+for (k = 0; k < sessionTable.length; k++) {
     tableColour(k);
 }
 
 // changing the expand text
 $('.expand-button').html('Expand')
-$('.expand-button').on('click', function() {
+$('.expand-button').on('click', function () {
     console.log(this.text)
     if (this.text == 'Expand') {
         $(this).html('Close');
@@ -67,7 +67,7 @@ $('.expand-button').on('click', function() {
 })
 
 // save the user's unit choice to the local storage
-$('#unit').on('click', function() {
+$('#unit').on('click', function () {
     var unit = $('#unit').val();
     localStorage.setItem('unit', unit);
 })
@@ -76,14 +76,53 @@ $('#unit').on('click', function() {
 var numberUnit = document.getElementsByClassName('check-number-float')
 
 function fixFloat() {
-    for(i=0; i < numberUnit.length; i++) {
+    for (i = 0; i < numberUnit.length; i++) {
         unitNum = numberUnit[i].textContent
         numberUnit[i].innerHTML = parseInt(unitNum);
     }
 }
 
-$('#training-session-id').on('click', function() {
+$('#training-session-id').on('click', function () {
     console.log('working2222')
 })
 
 
+// add session table
+var unitSelected = $('#session-unit-option option:selected').text()
+
+function addSessionTable() {
+    var typeSelected = $('#session-type-option option:selected').text()
+    unitSelected = $('#session-unit-option option:selected').text()
+    var powerliftingTable = `<div class="row p-0 m-0">
+    <div class="col-12">
+        <h3 class="my-3">Training Session</h3>
+    </div>
+    </div>
+    <table class="session-table container-fluid mt-3" id="addSessionTable">
+    <tr>
+        <th>Exercise:</th>
+        <th>Sets / Reps:</th>
+        <th>Weight:</th>
+        <th id="" class="unit-td"><i class="fas fa-balance-scale"></i></th>
+    </tr>
+    <tr>
+        <td><input class="container-fluid" type="text" name="session_exercise_1"></td>
+        <td><input class="container-fluid" type="type" name="session_sets_1"></td>
+        <td><input class="container-fluid" type="type" name="session_weight_1"></td>
+        <td class="unit-td">${unitSelected}</td>
+    </tr>
+    </table>
+    <a class="btn btn-dark text-light border my-2" onclick="createRow()">Add Row +</a>
+    <p class="m-0 p-0">Notes:</p>
+    <textarea name="notes" class="container-fluid form-control my-2"></textarea>
+    <button class="btn main-colour text-dark border float-right" type="submit" name="action">Done</button>`
+    console.log(typeSelected)
+    console.log(unitSelected)
+    if (typeSelected == 'Powerlifting') {
+        $('#addSessionTable').append(powerliftingTable)
+    }
+    for (k = 0; k < sessionTable.length; k++) {
+        tableColour(k);
+    }
+    $('#chooseTable').remove()
+}
