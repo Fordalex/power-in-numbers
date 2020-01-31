c52528e22ff884a162059861855c9abc1c2716a7
alex <alex96ford19@gmail.com>
2020-01-31 03:15:44 +0000

    changed the layout for the add session page, also added 'next' buttons so the page is filled out correctly

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -99,77 +99,70 @@ var unitSelected;
 var distanceSelected;
 
 function addSessionTable() {
+    $('#continueTableButton').remove()
     var typeSelected = $('#session-type-option option:selected').text()
     unitSelected = $('#session-unit-option option:selected').text()
     distanceSelected = $('#session-distance-option option:selected').text()
-    var powerliftingTable = `<div class="row p-0 m-0">
-    <div class="col-12 d-flex align-items-end">
-        <h3 class="float-left mr-3">Training Session</h3>
-        <h5 class="main-colour-text">Powerlifting</h5>
-    </div>
-    </div>
-    <table class="session-table container-fluid mt-3" id="session_table">
-    <tr>
-        <th>Exercise:</th>
-        <th>Sets / Reps:</th>
-        <th>Weight:</th>
-        <th class="unit-td"><i class="fas fa-balance-scale"></i></th>
-    </tr>
-    <tr>
-        <td><input class="container-fluid" type="text" name="session_exercise_1" required></td>
-        <td><input class="container-fluid" type="type" name="session_sets_1" required></td>
-        <td><input class="container-fluid" type="type" name="session_weight_1" required></td>
-        <td class="unit-td">${unitSelected}</td>
-    </tr>
-    </table>
-    <div class="row">
-        <div class="col-6 d-flex align-items-end">
-        <p class="m-0 p-0">Notes:</p>
-        </div>
-        <div class="col-6">
-        <a class="btn btn-dark text-light border my-3 float-right" onclick="createRow()">Add Row +</a>
-        </div>
-    </div>
-    
-    <textarea name="notes" class="container-fluid float-left form-control mb-3"></textarea>
-    <div class="row d-flex justify-content-end">
+    var powerliftingTable = `<div class="row p-0 m-0"  id="sessionTable">
+                                <div class="col-12 d-flex align-items-end p-0">
+                                    <h3 class="float-left mr-3">Training Session</h3>
+                                    <h5 class="main-colour-text">Powerlifting</h5>
+                                </div>
+                                <div class="col-12 p-0">
+                                    <table class="session-table container-fluid mt-3" id="session_table">
+                                        <tr>
+                                            <th>Exercise:</th>
+                                            <th>Sets / Reps:</th>
+                                            <th>Weight:</th>
+                                            <th class="unit-td"><i class="fas fa-balance-scale"></i></th>
+                                        </tr>
+                                        <tr>
+                                            <td><input class="container-fluid" type="text" name="session_exercise_1" required></td>
+                                            <td><input class="container-fluid" type="type" name="session_sets_1" required></td>
+                                            <td><input class="container-fluid" type="type" name="session_weight_1" required></td>
+                                            <td class="unit-td">${unitSelected}</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="col-6 d-flex align-items-end">
+                                    <p class="m-0 p-0">Notes:</p>
+                                </div>
+                                <div class="col-6 p-0">
+                                    <a class="btn btn-dark text-light border my-3 float-right" onclick="createRow()">Add Row +</a>
+                                </div>
+                                <div class="col-12 p-0">
+                                    <textarea name="notes" class="container-fluid float-left form-control mb-3"></textarea>
+                                </div>
+                                <div class="col-12 p-0">
+                                    <button class="btn main-colour container-fluid text-dark border float-right mb-3" type="submit" name="action">Done</button>
+                                </div>
+                            </div>`
 
-    <div class="col-6">
-    <button class="btn main-colour container-fluid text-dark border float-right" type="submit" name="action">Done</button>
-    </div>
-    </div>`
-    var runningTable = `<div class="row p-0 m-0">
-    <div class="col-12 d-flex align-items-end">
-    <h3 class="float-left mr-3">Training Session</h3>
-    <h5 class="main-colour-text">Running</h5>
-    </div>
-    </div>
-    <table class="session-table container-fluid mt-3" id="addSessionTable">
-    <tr>
-        <th>Distance (${distanceSelected}) :</th>
-    </tr>
-    <tr>
-        <td><input class="container-fluid" type="text" name="distance"></td>
-    </tr>
-    </table>
-    <div class="row">
-        <div class="col-6 d-flex align-items-end">
-        <p class="m-0 p-0">Notes:</p>
-        </div>
-        <div class="col-6">
-        <a class="btn btn-dark text-light border my-3 float-right" onclick="createRow()">Add Row +</a>
-        </div>
-    </div>
-    
-    <textarea name="notes" class="container-fluid float-left form-control mb-3"></textarea>
-    <div class="row">
-    <div class="col-6 pr-0">
-  
-    </div>
-    <div class="col-6">
-    <button class="btn main-colour container-fluid text-dark border float-right" type="submit" name="action">Done</button>
-    </div>
-    </div>`
+    var runningTable = `<div class="row p-0 m-0" id="sessionTable">
+                            <div class="col-12 d-flex align-items-end p-0">
+                                <h3 class="float-left mr-3">Training Session</h3>
+                                <h5 class="main-colour-text">Running</h5>
+                            </div>
+                            <div class="col-12 p-0">
+                                <table class="session-table container-fluid mt-3" id="addSessionTable">
+                                    <tr>
+                                        <th>Distance (${distanceSelected}) :</th>
+                                    </tr>
+                                    <tr>
+                                        <td><input class="container-fluid" type="text" name="distance"></td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="col-6 d-flex align-items-end">
+                                <p class="m-0 p-0 mt-2">Notes:</p>
+                            </div>
+                            <div class="col-12 p-0">
+                                <textarea name="notes" class="container-fluid float-left form-control mb-3"></textarea>
+                            </div>
+                            <div class="col-12 p-0">
+                                <button class="btn main-colour container-fluid text-dark border float-right mb-3" type="submit" name="action">Done</button>
+                            </div>
+                        </div>`
     console.log(typeSelected)
     console.log(unitSelected)
     if (typeSelected == 'Powerlifting') {
@@ -184,23 +177,58 @@ function addSessionTable() {
 
 // change to unit or distance depending on the users choice of session type
 
-$('#session-type-option').on('mouseleave', function  () {
-    usersChoice = $(this).val();
+$('#session-type-option').on('mouseleave', function () {
+
+
+
+});
+
+
+// add session form next buttons
+
+function addSessionFirstNext() {
+    usersChoice = $('#session-type-option').val();
     if (usersChoice == 'powerlifting') {
         $('#unitOrDistance').html(`
-        <label>Unit:</label>
-        <select id="session-unit-option" name="session_unit" class="form-control container-fluid" required>
-            <option value="kg">KG</option>
-            <option value="lb">LB</option>
-        </select>
+        <div id="unitContainer">
+            <label>Unit:</label>
+            <select onclick="unitContinueButton()" id="session-unit-option" name="session_unit" class="form-control container-fluid" required>
+                <option value="kg">KG</option>
+                <option value="lb">LB</option>
+            </select>
+            <div id="secondNextContainer">
+                <div class="btn btn-dark container-fluid my-3" onclick="addSessionTable()" id="continueTableButton">Next</div>
+            </div>
+        </div>
         `)
     } else if (usersChoice == 'running') {
         $('#unitOrDistance').html(`
-        <label>Distance:</label>
-        <select id="session-distance-option" name="session_unit" class="form-control container-fluid" required>
-            <option value="miles">Miles</option>
-            <option value="km">km</option>
-        </select>
+        <div id="unitContainer">
+            <label>Distance:</label>
+            <select onclick="unitContinueButton()" id="session-distance-option" name="session_unit" class="form-control container-fluid" required>
+                <option value="miles">Miles</option>
+                <option value="km">km</option>
+            </select>
+            <div id="secondNextContainer">
+                <div class="btn btn-dark container-fluid my-3" onclick="addSessionTable()" id="continueTableButton">Next</div>
+            </div>
+        </div>
         `)
     }
-});
+    $('#firstNextButton').remove()
+}
+
+function unitContinueButton() {
+    $('#secondNextContainer').html(`<div class="btn btn-dark container-fluid my-3" onclick="addSessionTable()" id="continueTableButton">Next</div>`)
+
+}
+
+function backToFormStart() {
+    $('#firstNextButtonContainer').html(`
+    <button id="firstNextButton" class="btn btn-dark unit-button my-3 container-fluid px-0"
+    name="unit" onclick="addSessionFirstNext()" >Next</button>
+    `)
+    $('#unitContainer').remove()
+    $('#sessionTable').remove()
+}
+
